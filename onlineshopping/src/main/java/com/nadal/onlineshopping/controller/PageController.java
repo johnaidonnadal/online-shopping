9b4d605c219bc8ea887eb15e72d793ad78496851
johnaidonnadal <johnaidon.nadal@gmail.com>
2018-09-26 01:51:50 +0800

    Creating Master Page

--- onlineshopping/src/main/java/com/nadal/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/nadal/onlineshopping/controller/PageController.java
@@ -11,7 +11,38 @@ public class PageController {
 	public ModelAndView index() {
 		
 		ModelAndView modelView = new ModelAndView("page");
-		modelView.addObject("greeting", "Welcome to Spring MVC");
+		modelView.addObject("title", "Home");
+		modelView.addObject("userClickHome", true);
+		
+		return modelView;
+	}
+	
+	@RequestMapping(value= "about")
+	public ModelAndView about() {
+		
+		ModelAndView modelView = new ModelAndView("page");
+		modelView.addObject("title", "AboutUs");
+		modelView.addObject("userClickAbout", true);
+		
+		return modelView;
+	}
+	
+	@RequestMapping(value= "products")
+	public ModelAndView products() {
+		
+		ModelAndView modelView = new ModelAndView("page");
+		modelView.addObject("title", "Products");
+		modelView.addObject("userClickProduct", true);
+		
+		return modelView;
+	}
+	
+	@RequestMapping(value= "contact")
+	public ModelAndView contact() {
+		
+		ModelAndView modelView = new ModelAndView("page");
+		modelView.addObject("title", "Contact");
+		modelView.addObject("userClickContact", true);
 		
 		return modelView;
 	}
